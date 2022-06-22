712ee3afa94e5b6be6e03c534f8e67004beed320
Disaylt <disofban@gmail.com>
2022-06-22 18:01:58 +0300

    create session builder

---
+++ ZennoPosterProjectAccountRegister/AccountStore/SessionBuilder.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZennoPosterProjectAccountRegister.AccountStore
+{
+    internal class SessionBuilder
+    {
+        public List<char> CharacterSet { get; } = new List<char>();
+
+        public SessionBuilder(bool isUseLetters, bool isUseNumber)
+        {
+            if(isUseLetters)
+            {
+                CharacterSet.AddRange(GetLetters());
+            }
+            if(isUseNumber)
+            {
+                CharacterSet.AddRange(GetNumbers());
+            }
+        }
+
+        public string CreateSessionName(int size)
+        {
+            int charSum = CharacterSet.Count;
+            if (charSum != 0)
+            {
+                Random random = new Random();
+                char[] letters = Enumerable.Range(0, size)
+                    .Select(i => CharacterSet[random.Next(0, charSum)])
+                    .ToArray();
+                return new string(letters);
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+
+        private char[] GetNumbers()
+        {
+            char[] numbers = Enumerable.Range('0', 10)
+                .Select(i => (char)i)
+                .ToArray();
+            return numbers;
+        }
+
+        private char[] GetLetters()
+        {
+            char[] letters = Enumerable.Range('a','z' - 'a' + 1)
+                .Select(i => (char)i)
+                .ToArray();
+            return letters;
+        }
+    }
+}

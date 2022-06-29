f2eb0854f8d653e256910ec8a667f71bf061cbe8
Disaylt <disofban@gmail.com>
2022-06-29 12:30:11 +0300

    add htppclient for account cheker

--- ZennoPosterProjectAccountRegister/WB/WbRegisterChecker.cs
+++ ZennoPosterProjectAccountRegister/WB/WbRegisterChecker.cs
@@ -1,18 +1,23 @@
-﻿using System;
+﻿using SmartProxyV2_ZennoLabVersion.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ZennoLab.InterfacesLibrary.ProjectModel;
 using ZennoPosterProjectAccountRegister.AccountStore;
+using ZennoPosterProjectAccountRegister.Http;
 
 namespace ZennoPosterProjectAccountRegister.WB
 {
     internal class WbRegisterChecker
     {
         private readonly Account _account;
-        public WbRegisterChecker(Account acount)
+        private readonly HttpSenderClient _httpSenderClient;
+        public WbRegisterChecker(Account account, ProxyModel proxy, IZennoPosterProjectModel project)
         {
-            _account = acount;
+            _account = account;
+            _httpSenderClient = new HttpSenderClient(project, proxy);
         }
 
         public bool IsRegistred()

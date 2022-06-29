e3e8ea0275838d7ced6f5a19567ad36b22bbdaf1
Disaylt <disofban@gmail.com>
2022-06-29 12:25:16 +0300

    create RegisterChecker

---
+++ ZennoPosterProjectAccountRegister/WB/WbRegisterChecker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZennoPosterProjectAccountRegister.AccountStore;
+
+namespace ZennoPosterProjectAccountRegister.WB
+{
+    internal class WbRegisterChecker
+    {
+        private readonly Account _account;
+        public WbRegisterChecker(Account acount)
+        {
+            _account = acount;
+        }
+
+        public bool IsRegistred()
+        {
+
+        }
+    }
+}

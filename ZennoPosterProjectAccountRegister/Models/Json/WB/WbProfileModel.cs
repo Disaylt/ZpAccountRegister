96551a9a08e0b2536d20defb02762e811a1297f6
Disaylt <disofban@gmail.com>
2022-06-29 14:50:13 +0300

    create profile model

---
+++ ZennoPosterProjectAccountRegister/Models/Json/WB/WbProfileModel.cs
@@ -0,0 +1,27 @@
+﻿using Global.ZennoLab.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZennoPosterProjectAccountRegister.Models.Json.WB
+{
+    public class WbProfileModel
+    {
+        [JsonProperty("value")]
+        public WbProfile ProfileData { get; set; }
+    }
+
+    public class WbProfile
+    {
+        [JsonProperty("firstName")]
+        public string FirstName { get; set; }
+
+        [JsonProperty("lastName")]
+        public string LastName { get; set; }
+
+        [JsonProperty("isAuthenticated")]
+        public bool IsAuthenticated { get; set; }
+    }
+}

06b8d30372b3a0f7cb8a3a3135315bd073161746
Disaylt <disofban@gmail.com>
2022-06-29 16:45:41 +0300

    add WbAccountSettingsModel

---
+++ ZennoPosterProjectAccountRegister/Models/Json/WB/WbAccountSettingsModel.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZennoPosterProjectAccountRegister.Models.Json.WB
+{
+    internal class WbAccountSettingsModel
+    {
+        [JsonProperty("value")]
+        public WbAccountSettingsValue Value { get; set; }
+
+    }
+
+    internal class WbAccountSettingsValue
+    {
+        [JsonProperty("data")]
+        public WbAccountSettingsData Data { get; set; }
+    }
+
+    internal class WbAccountSettingsData
+    {
+        [JsonProperty("mySafety")]
+        WbAccountSettingsMySafety MySafety { get; set; }
+    }
+
+    internal class WbAccountSettingsMySafety
+    {
+        [JsonProperty("sessions")]
+        List<WbAccountSettingsSession> Sessions { get; set; }
+    }
+
+    internal class WbAccountSettingsSession
+    {
+        [JsonProperty("sessionId")]
+        public string SessionId { get; set; }
+    }
+}

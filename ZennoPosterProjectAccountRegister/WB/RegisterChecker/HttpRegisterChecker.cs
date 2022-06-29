da0a66e3395ec34e55725ed37862cda4bdcd4869
Disaylt <disofban@gmail.com>
2022-06-29 14:37:17 +0300

    create httpRegisterChecker

---
+++ ZennoPosterProjectAccountRegister/WB/RegisterChecker/HttpRegisterChecker.cs
@@ -0,0 +1,24 @@
+﻿using SmartProxyV2_ZennoLabVersion.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZennoLab.InterfacesLibrary.ProjectModel;
+using ZennoPosterProjectAccountRegister.Http;
+
+namespace ZennoPosterProjectAccountRegister.WB.RegisterChecker
+{
+    internal class HttpRegisterChecker : HttpSenderClient
+    {
+        public HttpRegisterChecker(ProxyModel proxy, IZennoPosterProjectModel project) : base(project, proxy)
+        {
+
+        }
+
+        public Task<object> GetProfileInfo()
+        {
+
+        }
+    }
+}

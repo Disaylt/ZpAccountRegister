5ced4fa6f3878126184ee74e087bd672acd2d95f
Disaylt <disofban@gmail.com>
2022-06-30 11:06:28 +0300

    create rest proxy

---
+++ ZennoPosterProjectAccountRegister/Tests/Proxy/TestProxy.cs
@@ -0,0 +1,21 @@
+﻿using SmartProxyV2_ZennoLabVersion;
+using SmartProxyV2_ZennoLabVersion.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZennoPosterProjectAccountRegister.Tests.Proxy
+{
+    internal class TestProxy
+    {
+        public static ProxyModel GetTestProxy()
+        {
+            ProxyModel proxyModel = new ProxyModel("log", "pass", "proxy.soax.com");
+            ProxyPort port = new ProxyPort("wb", 9000);
+            proxyModel.PortData = port;
+            return proxyModel;
+        }
+    }
+}

0c64a5437f4a8ca25780aa72465d303296ceffa3
Disaylt <disofban@gmail.com>
2022-07-13 13:15:00 +0300

    create model

---
+++ ZennoPosterProjectAccountRegister/Models/Json/OnlineSim/ServiceModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZennoPosterProjectAccountRegister.Models.Json.OnlineSim
+{
+    internal class ServiceModel
+    {
+        public int Count { get; set; }
+        public bool Popular { get; set; }
+        public int Code { get; set; }
+        public int Id { get; set; }
+        public string Service { get; set; }
+    }
+}

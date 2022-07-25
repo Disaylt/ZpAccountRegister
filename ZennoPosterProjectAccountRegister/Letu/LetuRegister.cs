20f5156e710f11695f128d001431930b55e0fde2
Disaylt <disofban@gmail.com>
2022-07-25 17:49:14 +0300

    Create register controller

---
+++ ZennoPosterProjectAccountRegister/Letu/LetuRegister.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZennoLab.CommandCenter;
+using ZennoLab.InterfacesLibrary.ProjectModel;
+using ZennoPosterProjectAccountRegister.AccountStore;
+
+namespace ZennoPosterProjectAccountRegister.Letu
+{
+    internal class LetuRegister : RegisterController
+    {
+        public LetuRegister(Instance instance, IZennoPosterProjectModel project) : base(instance, project)
+        {
+
+        }
+
+        public override Account Account => throw new NotImplementedException();
+
+        public override void StartRegistration()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

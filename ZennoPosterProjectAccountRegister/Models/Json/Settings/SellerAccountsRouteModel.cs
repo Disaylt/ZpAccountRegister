baa8422db7fe6f776799d0b8bf37fc90f5f0e2df
Disaylt <disofban@gmail.com>
2022-08-01 12:58:29 +0300

    create new route model, delete path from settings

---
+++ ZennoPosterProjectAccountRegister/Models/Json/Settings/SellerAccountsRouteModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZennoPosterProjectAccountRegister.Models.Json.Settings
+{
+    internal class SellerAccountsRouteModel
+    {
+        public string SellerName { get; set; }
+        public string PathForSaveGoodAccount { get; set; }
+        public string PathForSaveBadAccount { get; set; }
+    }
+}
